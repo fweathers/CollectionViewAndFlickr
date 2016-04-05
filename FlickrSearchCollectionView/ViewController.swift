c2aa41a08b188fbd1c4974d010516c2e80134cbd
fweathers <fel.weathers@gmail.com>
2016-04-05 13:31:35 -0400

    Initial Commit

---
+++ FlickrSearchCollectionView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlickrSearchCollectionView
+//
+//  Created by Felicia Weathers on 4/5/16.
+//  Copyright © 2016 Felicia Weathers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
