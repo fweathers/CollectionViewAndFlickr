1510937bb6e45c2057f84564a1505863a3f004cf
fweathers <fel.weathers@gmail.com>
2016-04-06 00:01:55 -0400

    First commit

---
+++ FlickrSearchCollectionView/FlickrPhotoCell.swift
@@ -0,0 +1,14 @@
+//
+//  FlickrPhotoCell.swift
+//  FlickrSearchCollectionView
+//
+//  Created by Felicia Weathers on 4/5/16.
+//  Copyright © 2016 Felicia Weathers. All rights reserved.
+//
+
+import UIKit
+
+class FlickrPhotoCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+}
